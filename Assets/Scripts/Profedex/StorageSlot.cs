377c84670d4ca748d3cef91f7d54bbb6ceda4521
ItsMeZeVas <veralegrojuansebastian@gmail.com>
2026-03-14 09:03:31 -0500

    Movimiento entre Party y Storage

--- Assets/Scripts/Profedex/StorageSlot.cs
+++ Assets/Scripts/Profedex/StorageSlot.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
@@ -15,5 +15,38 @@ public class StorageSlot : MonoBehaviour
 
         icon.sprite = p.data.image;
         nameText.text = p.data.professorName;
+    }
+
+    public void OnClick()
+    {
+        PlayerPartyManager manager = PlayerPartyManager.Instance;
+
+        if (manager.party.Contains(instance))
+        {
+            // Mover de PARTY → STORAGE
+            manager.party.Remove(instance);
+            manager.storage.Add(instance);
+
+            Debug.Log(instance.data.professorName + " movido a Storage");
+        }
+        else
+        {
+            // Mover de STORAGE → PARTY
+            if (manager.party.Count >= manager.maxPartySize)
+            {
+                Debug.Log("Party llena");
+                return;
+            }
+
+            manager.storage.Remove(instance);
+            manager.party.Add(instance);
+
+            Debug.Log(instance.data.professorName + " movido a Party");
+        }
+
+        StorageMenuManager menu = FindObjectOfType<StorageMenuManager>();
+
+        if (menu != null)
+            menu.Refresh();
     }
 }
