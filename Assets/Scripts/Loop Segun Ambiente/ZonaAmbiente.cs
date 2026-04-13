c37ec908068b2f0f97800deaa3555e1c128534a0
Nicolás Arturo Rodríguez Varela <est.nicolasa.rodri2@unimilitar.edu.co>
2026-04-13 18:01:08 -0500

    Update ZonaAmbiente.cs

--- Assets/Scripts/Loop Segun Ambiente/ZonaAmbiente.cs
+++ Assets/Scripts/Loop Segun Ambiente/ZonaAmbiente.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ZonaAmbiente : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Eventos al entrar")]
+    public UnityEvent OnEnterZona;
+
+    [Header("Eventos al salir")]
+    public UnityEvent OnExitZona;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            OnEnterZona.Invoke();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            OnExitZona.Invoke();
+        }
     }
 }
